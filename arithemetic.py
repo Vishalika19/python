5f0fbc2c9e781e2954738160d7a0c05f619ee7d3
Vishalika19 <vishalikaledalla@gmail.com>
2025-10-30 16:09:25 +0530

    python

---
+++ arithemetic.py
@@ -0,0 +1,11 @@
+x=10
+y=5
+print("x:",x)
+print("y:",y)
+print("addition:",x+y)
+print("subtraction:",x-y)
+print("multiplication:",x*y)
+print("division:",x/y)
+print("floor division:",x//y)
+print("modulas:",x%y)
+print("power:",x**y)
